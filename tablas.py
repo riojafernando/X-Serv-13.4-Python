1c3448988db10e518772c415925e3a49406ea4f3
fernando rioja checa <frioja@lambda28.aulas.gsyc.es>
2015-01-27 10:05:06 +0100

    Subiendo tablas

---
+++ tablas.py
@@ -0,0 +1,7 @@
+
+rango = range(1,11)
+for numero in rango:
+    for elemento in rango:
+        resultado = numero * elemento 
+        print (str(numero) + ' X ' + str(elemento) + ' = ' + str(resultado))
+    
